34179f29dea8dd0a7a5969534b7f326cd7288d05
xiaojun1994 <362896731@qq.com>
2019-06-21 13:22:52 +0800

    小修整

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const Key = '__VUEX_STATE__'
+
 export default function(store) {
-  if (localStorage.vuexState) store.replaceState(JSON.parse(localStorage.vuexState));
+  if (localStorage[key]) store.replaceState(JSON.parse(localStorage[Key]));
   store.subscribe(function(mutation, state) {
-    localStorage.setItem('vuexState', JSON.stringify(state));
+    localStorage.setItem(Key, JSON.stringify(state));
   });
 }
